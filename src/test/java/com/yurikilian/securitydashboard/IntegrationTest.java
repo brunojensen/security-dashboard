a17548a7659cff41f6ed924c044c46dc25101a86
Yuri Kilian <kilian.inf@gmail.com>
2018-02-25 16:10:25 -0300

    //tests
    Signed-off-by: Yuri Kilian <kilian.inf@gmail.com>

--- src/test/java/com/yurikilian/securitydashboard/IntegrationTest.java
+++ src/test/java/com/yurikilian/securitydashboard/IntegrationTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
@@ -14,18 +15,24 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.codec.Base64;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import com.yurikilian.securitydashboard.domain.User;
 import com.yurikilian.securitydashboard.util.RestResponsePage;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@TestPropertySource(locations = "classpath:application.properties")
 public class IntegrationTest {
+
+  @Value("${liquibase.parameters.initialUsername}")
+  private String username;
+
+  @Value("${application.initialPassword}")
+  private String password;
 
   @Autowired
   private TestRestTemplate restTemplate;
-
-
 
   @Test
   public void shouldReturn401WhenNoCredentialsInformed() {
@@ -39,26 +46,20 @@ public class IntegrationTest {
     // arrange
     HttpHeaders headers = generateBasicAuthorizationHeader();
     HttpEntity<String> request = new HttpEntity<String>(headers);
-    // act
+
     ResponseEntity<RestResponsePage<User>> response = restTemplate.exchange("/user", HttpMethod.GET,
         request, new ParameterizedTypeReference<RestResponsePage<User>>() {});
+
     assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
     assertThat(response.getBody()).isNotNull();
-
-    // assert
 
   }
 
   private HttpHeaders generateBasicAuthorizationHeader() {
-    HttpHeaders headers = new HttpHeaders() {
-      private static final long serialVersionUID = 4817192906171648043L;
-      {
-        String authHeader =
-            "Basic " + new String(Base64.encode("kilian.inf@gmail.com:admin".getBytes()));
-        set("Authorization", authHeader);
-      }
-    };
-    return headers;
+    HttpHeaders httpHeaders = new HttpHeaders();
+    httpHeaders.set("Authorization",
+        "Basic " + new String(Base64.encode((this.username + ":" + this.password).getBytes())));
+    return httpHeaders;
   }
 
 
